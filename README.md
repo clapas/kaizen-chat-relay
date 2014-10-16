7cefff3ee4d9bca50f4b81c94ebf9ec7de53fa8f
Claudio Pascual <claudio.pascual@gmail.com>
2014-10-16 15:14:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+kaizen-chat-relay
+=================
+
+A nodejs chat relay to act as a gateway between web browser and XMPP bot
---
+++ index.js
@@ -0,0 +1,87 @@
+var express = require('express')
+var app = express();
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send("Hi there!")
+})
+
+var fs = require('fs'); // this engine requires the fs module
+app.engine('ntl', function (filePath, options, callback) { // define the template engine
+    fs.readFile(filePath, function (err, content) {
+        if (err) throw new Error(err);
+        // this is an exteremly simple template engine
+        var rendered = content.toString()
+            .replace('#bgcolor#', options.bgcolor)
+            .replace('#chat_relay_baseURL#', options.baseURL);
+        return callback(null, rendered);
+    })
+});
+app.set('views', './views'); // specify the views directory
+app.set('view engine', 'ntl'); // register the template engine
+
+app.get('/appkaizen.js', function (req, res) {
+    if (!req.query.bgcolor) req.query.bgcolor = '#ddd';
+    res.setHeader('content-type', 'application/javascript');
+    res.render('appkaizen', {
+        bgcolor: req.query.bgcolor,
+        baseURL: req.protocol + '://' + req.get('host')
+    });
+});
+
+io.on('connection', function(socket) {
+    var options = {
+        host: 'appkaizen.appspot.com',
+        port: 80,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    };
+    socket.on('disconnect', function () {
+        var data = 'socket.io=' + socket.id + '&user_id=' + socket.user_id;
+
+        options.path = '/disconnect';
+        options.headers['Content-Length'] = Buffer.byteLength(data);
+        var httpreq = http.request(options, function (response) {
+            response.setEncoding('utf8');
+            response.on('data', function (chunk) {
+                console.log("body: " + chunk);
+            });
+        });
+        httpreq.write(data);
+        httpreq.end();
+    });
+    socket.on('chat message', function(data) {
+        console.log(data, socket.id);
+        socket.user_id = data.user_id;
+        //io.sockets.in(socket.id).emit('chat message', data.message);
+        data.message = 'message=' + encodeURIComponent(data.message) + '&socket.io=' + socket.id + '&user_id=' + data.user_id;
+
+        options.path = '/send';
+        options.headers['Content-Length'] = Buffer.byteLength(data.message);
+        var httpreq = http.request(options, function (response) {
+            response.setEncoding('utf8');
+            response.on('data', function (chunk) {
+                console.log("body: " + chunk);
+            });
+        });
+        httpreq.write(data.message);
+        httpreq.end();
+    });
+});
+
+app.get('/answer', function(req, res) {
+    console.log(req.query.message + ' answered');
+    io.sockets.in(req.query['socket.io']).emit('chat message', req.query.message);
+    res.send('ok');
+});
+
+server.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
